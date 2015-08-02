83eb1a7b124e751f67fa0593823ab067f7fb99c4
Johnny Shieh <xiaoxie17@gmail.com>
2015-08-02 10:12:31 +0800

    support convert single file

--- bs.py
+++ bs.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 """
 Remove BOM(Byte Order Marker) from utf-8 files.
 """
@@ -12,28 +15,36 @@ def init_options():
     parser.add_argument('--type', dest='type', help='file type')
     return parser.parse_args()
 
+def remove_bom(filepath, filetype):
+    if filetype == None or filepath.endswith('.' + filetype):
+        with open(filepath, 'r+b') as fp:
+            chunk = fp.read(BUFSIZE)
+            if chunk.startswith(codecs.BOM_UTF8):
+                i = 0
+                chunk = chunk[BOMLEN:]
+                while chunk:
+                    fp.seek(i)
+                    fp.write(chunk)
+                    i += len(chunk)
+                    fp.seek(BOMLEN, os.SEEK_CUR)
+                    chunk = fp.read(BUFSIZE)
+                fp.seek(-BOMLEN, os.SEEK_CUR)
+                fp.truncate()
+                print('Converted: ' + filepath)
+            else:
+                print(filepath + " file_encoding is utf8 without BOM.")
+
 def main():
     args = init_options()
     path = args.path
     file_type = args.type
-
-    for root, dirs, files in os.walk(path):
-        for f in files:
-            if file_type != None and f.endswith('.' + file_type):
-                with open(root + '/' + f, 'r+b') as fp:
-                    chunk = fp.read(BUFSIZE)
-                    if chunk.startswith(codecs.BOM_UTF8):
-                        i = 0
-                        chunk = chunk[BOMLEN:]
-                        while chunk:
-                            fp.seek(i)
-                            fp.write(chunk)
-                            i += len(chunk)
-                            fp.seek(BOMLEN, os.SEEK_CUR)
-                            chunk = fp.read(BUFSIZE)
-                        fp.seek(-BOMLEN, os.SEEK_CUR)
-                        fp.truncate()
-                        print 'Converted: ' + root + f
+    
+    if os.path.isfile(path):
+        remove_bom(path, file_type)
+    else:
+        for root, dirs, files in os.walk(path):
+            for f in files:
+                remove_bom(root + '/' + f, file_type)
 
 if __name__ == '__main__':
     main()
